55332d0eb9127b88259710d5b3d8c205836aa6e6
unknown <pismecho@gmail.com>
2021-04-29 16:10:21 +0500

    Сделал тестовое задание

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
 import logo from './logo.svg';
-import './App.css';
+import s from './App.module.scss';
+import card from './news_foto.jpg'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={s.App}>
+        <div className={s.card}>
+            <h3 className={s.cardLabel}>Семантика в верстке</h3>
+            <div className={s.content}>
+                <div className={s.img}><img src={card} alt=""/></div>
+                <div className={s.info}>
+                    <h6 className={s.infoLabel}><a href="">htmlbook</a> - <span>5 января 2014</span></h6>
+                    <p>Семантика дает представление о структуре документа и позволяет людям и программам более полно управлять данными. В HTML5 добавлено множество семантических тегов, а также поддержка RDFa, микроформаторов и микроданных ...</p>
+                </div>
+                <a href="" className={s.readMoreLink}>ЧИТАТЬ ДАЛЬШЕ</a>
+            </div>
+
+        </div>
+        <div className={s.card}>
+            <h3 className={s.cardLabel}>Семантика в верстке</h3>
+            <div className={s.content}>
+                <div className={s.img}><img src={card} alt=""/></div>
+                <div className={s.info}>
+                    <h6 className={s.infoLabel}><a href="">htmlbook</a> - <span>5 января 2014</span></h6>
+                    <p>Семантика дает представление о структуре документа и позволяет людям и программам более полно управлять данными. В HTML5 добавлено множество семантических тегов, а также поддержка RDFa, микроформаторов и микроданных ...</p>
+                </div>
+                <a href="" className={s.readMoreLink}>ЧИТАТЬ ДАЛЬШЕ</a>
+            </div>
+
+        </div>
+        <div className={s.card}>
+            <h3 className={s.cardLabel}>Семантика в верстке</h3>
+            <div className={s.content}>
+                <div className={s.img}><img src={card} alt=""/></div>
+                <div className={s.info}>
+                    <h6 className={s.infoLabel}><a href="">htmlbook</a> - <span>5 января 2014</span></h6>
+                    <p>Семантика дает представление о структуре документа и позволяет людям и программам более полно управлять данными. В HTML5 добавлено множество семантических тегов, а также поддержка RDFa, микроформаторов и микроданных ...</p>
+                </div>
+                <a href="" className={s.readMoreLink}>ЧИТАТЬ ДАЛЬШЕ</a>
+            </div>
+
+        </div>
+        <div className={s.card}>
+            <h3 className={s.cardLabel}>Семантика в верстке</h3>
+            <div className={s.content}>
+                <div className={s.img}><img src={card} alt=""/></div>
+                <div className={s.info}>
+                    <h6 className={s.infoLabel}><a href="">htmlbook</a> - <span>5 января 2014</span></h6>
+                    <p>Семантика дает представление о структуре документа и позволяет людям и программам более полно управлять данными. В HTML5 добавлено множество семантических тегов, а также поддержка RDFa, микроформаторов и микроданных ...</p>
+                </div>
+                <a href="" className={s.readMoreLink}>ЧИТАТЬ ДАЛЬШЕ</a>
+            </div>
+
+        </div>
+        <div className={s.card}>
+            <h3 className={s.cardLabel}>Семантика в верстке</h3>
+            <div className={s.content}>
+                <div className={s.img}><img src={card} alt=""/></div>
+                <div className={s.info}>
+                    <h6 className={s.infoLabel}><a href="">htmlbook</a> - <span>5 января 2014</span></h6>
+                    <p>Семантика дает представление о структуре документа и позволяет людям и программам более полно управлять данными. В HTML5 добавлено множество семантических тегов, а также поддержка RDFa, микроформаторов и микроданных ...</p>
+                </div>
+                <a href="" className={s.readMoreLink}>ЧИТАТЬ ДАЛЬШЕ</a>
+            </div>
+
+        </div>
     </div>
   );
 }
